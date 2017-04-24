73d7c526f9babdb61ce57e6386919b8552ab4f3e
Cauldron <batocode123@gmail.com>
2017-04-25 09:41:17 +1000

    ready for some early refactoring - branch

--- game.js
+++ game.js
@@ -20,12 +20,14 @@ new Vue({
 			this.mstrAtk = Math.floor(Math.random() *10);
 			this.playerPoints = this.playerPoints - this.mstrAtk;
 			this.plyrPointsWidth = this.playerPoints/100*350;
-			this.plyrAtk = Math.floor(Math.random() * 10);
+			this.gameLog.push('Monster hits you with ' + this.mstrAtk + 'points of damage');
+			this.plyrAtk = Math.floor(Math.random() * 20);
 			this.monsterPoints = this.monsterPoints - this.plyrAtk;
 			this.mstrPointsWidth = this.monsterPoints/100*350;
+			this.gameLog.push('You hit the monster with ' + this.plyrAtk + 'points of damage');
 		},
 		specialAttack: function(){
-		this.plyrAtk = Math.floor(Math.random() * 10)*2;
+		this.plyrAtk = Math.floor(Math.random() * 20)*2;
 			this.monsterPoints = this.monsterPoints - this.plyrAtk;
 			this.mstrPointsWidth = this.monsterPoints/100*350;
 		}
